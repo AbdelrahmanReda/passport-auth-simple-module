dfd8d8362710564f3d4cc4ad4d6864fe6e9995e7
abdelrahman-reda <boodycat09@gmail.com>
2024-02-28 23:47:06 +0200

    change cors config

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // 'http://localhost:3000', 'https://example.com
+    origin: process.env.CLIENT_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
