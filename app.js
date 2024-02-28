cdc8e05a4e5a2a79011ee8b088a4a4d5ed24c57c
abdelrahman-reda <boodycat09@gmail.com>
2024-02-28 23:55:38 +0200

    change cors config

--- app.js
+++ app.js
@@ -76,10 +76,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin:
-      process.env.NODE_ENV === "production"
-        ? "https://passport-auth-simple-module.onrender.com"
-        : "http://localhost:3000",
+    origin: "http://localhost:3000",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
